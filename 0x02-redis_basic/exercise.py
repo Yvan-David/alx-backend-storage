9ffd80408af0d3eaa15f51b6f93bdaece2daee37
Yvan-David <irankundayvan2020@gmail.com>
2024-01-27 11:53:39 +0200

    setting up redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data) -> None:
         """ assignng key value pair """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
